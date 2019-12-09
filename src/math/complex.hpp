3384bddd905d5c8c7fc99e75c7ec214453337894
GabeRundlett <gabe@rundlett.com>
2019-12-09 12:14:31 -0500

    Fixed compile issues

--- src/math/complex.hpp
+++ src/math/complex.hpp
@@ -13,14 +13,16 @@ namespace math {
 
         inline constexpr auto operator+(const Complex<T, 4> &c) const { return Complex<T, 4>{r + c.r, i + c.i}; }
         inline constexpr auto operator-(const Complex<T, 4> &c) const { return Complex<T, 4>{r - c.r, i - c.i}; }
-        inline constexpr auto operator*(const Complex<T, 4> &c) const { return {r * c.r - i * c.i, r * c.i + i * c.r}; }
+        inline constexpr auto operator*(const Complex<T, 4> &c) const {
+            return Complex<T, 2>{r * c.r - i * c.i, r * c.i + i * c.r};
+        }
         inline constexpr auto operator/(const Complex<T, 4> &c) const {
             const T inverse_magnitude_sq = static_cast<T>(1) / (sq(c.r) + sq(c.i));
-            return {r * c.r * inverse_magnitude_sq + i * c.i * inverse_magnitude_sq,
-                    i * c.r * inverse_magnitude_sq - r * c.i * inverse_magnitude_sq};
+            return Complex<T, 2>{r * c.r * inverse_magnitude_sq + i * c.i * inverse_magnitude_sq,
+                                 i * c.r * inverse_magnitude_sq - r * c.i * inverse_magnitude_sq};
         }
-        inline constexpr auto operator*(const T a) const { return Complex<T, 4>{i * a, y * a}; }
-        inline constexpr auto operator/(const T a) const { return Complex<T, 4>{i / a, y / a}; }
+        inline constexpr auto operator*(const T a) const { return Complex<T, 4>{r * a, i * a}; }
+        inline constexpr auto operator/(const T a) const { return Complex<T, 4>{r / a, i / a}; }
         inline constexpr auto &operator+=(const Complex<T, 4> &c) {
             r += c.r, i += c.i;
             return *this;
@@ -51,7 +53,7 @@ namespace math {
         }
         inline constexpr auto &operator[](const unsigned int index) const { return data[index]; }
         inline constexpr auto dot(const Complex<T, 4> &c) const { return r * c.r + i * c.i; }
-        inline constexpr auto length() const { return static_cast<T>(INTERNAL_MATH_SQUARE_ROOT_FUNC(sq(x) + sq(y))); }
+        inline constexpr auto length() const { return static_cast<T>(INTERNAL_MATH_SQUARE_ROOT_FUNC(sq(r) + sq(i))); }
         inline constexpr auto &normalize() {
             T factor = static_cast<T>(1) / length();
             r *= factor, i *= factor;
@@ -74,22 +76,22 @@ namespace math {
             return Complex<T, 4>{r - c.r, i - c.i, j - c.j, k - c.k};
         }
         inline constexpr auto operator*(const Complex<T, 4> &c) const {
-            return {r * c.r - i * c.i - j * c.j - k * c.k, r * c.i + i * c.r + j * c.k - k * c.j,
-                    r * c.j - i * c.k + j * c.r + k * c.i, r * c.k + i * c.j - j * c.i + k * c.r};
+            return Complex<T, 4>{r * c.r - i * c.i - j * c.j - k * c.k, r * c.i + i * c.r + j * c.k - k * c.j,
+                                 r * c.j - i * c.k + j * c.r + k * c.i, r * c.k + i * c.j - j * c.i + k * c.r};
         }
         inline constexpr auto operator/(const Complex<T, 4> &c) const {
             const auto inverse_magnitude_sq = static_cast<T>(1) / (sq(c.r) + sq(c.i) + sq(c.j) + sq(c.k));
-            return {r * c.r * inverse_magnitude_sq + i * c.i * inverse_magnitude_sq + j * c.j * inverse_magnitude_sq +
-                        k * c.k * inverse_magnitude_sq,
-                    i * c.r * inverse_magnitude_sq - j * c.k * inverse_magnitude_sq + k * c.j * inverse_magnitude_sq -
-                        r * c.i * inverse_magnitude_sq,
-                    i * c.k * inverse_magnitude_sq + j * c.r * inverse_magnitude_sq - k * c.i * inverse_magnitude_sq -
-                        r * c.j * inverse_magnitude_sq,
-                    j * c.i * inverse_magnitude_sq + k * c.r * inverse_magnitude_sq - r * c.k * inverse_magnitude_sq -
-                        i * c.j * inverse_magnitude_sq};
+            return Complex<T, 4>{r * c.r * inverse_magnitude_sq + i * c.i * inverse_magnitude_sq +
+                                     j * c.j * inverse_magnitude_sq + k * c.k * inverse_magnitude_sq,
+                                 i * c.r * inverse_magnitude_sq - j * c.k * inverse_magnitude_sq +
+                                     k * c.j * inverse_magnitude_sq - r * c.i * inverse_magnitude_sq,
+                                 i * c.k * inverse_magnitude_sq + j * c.r * inverse_magnitude_sq -
+                                     k * c.i * inverse_magnitude_sq - r * c.j * inverse_magnitude_sq,
+                                 j * c.i * inverse_magnitude_sq + k * c.r * inverse_magnitude_sq -
+                                     r * c.k * inverse_magnitude_sq - i * c.j * inverse_magnitude_sq};
         }
-        inline constexpr auto operator*(const T a) const { return Complex<T, 4>{i * a, y * a, j * a, k * a}; }
-        inline constexpr auto operator/(const T a) const { return Complex<T, 4>{i / a, y / a, j / a, k / a}; }
+        inline constexpr auto operator*(const T a) const { return Complex<T, 4>{r * a, i * a, j * a, k * a}; }
+        inline constexpr auto operator/(const T a) const { return Complex<T, 4>{r / a, i / a, j / a, k / a}; }
         inline constexpr auto &operator+=(const Complex<T, 4> &c) {
             r += c.r, i += c.i, j += c.j, k += c.k;
             return *this;
@@ -99,7 +101,7 @@ namespace math {
             return *this;
         }
         inline constexpr auto &operator*=(const Complex<T, 4> &c) {
-            const T temp_r = r, temp_j = i, temp_j = j;
+            const T temp_r = r, temp_i = i, temp_j = j;
             r = r * c.r - i * c.i - j * c.j - k * c.k;
             i = temp_r * c.i + i * c.r + j * c.k - k * c.j;
             j = temp_r * c.j - temp_j * c.k + j * c.r + k * c.i;
@@ -115,7 +117,7 @@ namespace math {
                 temp_r * c.i * inverse_magnitude_sq;
             j = temp_i * c.k * inverse_magnitude_sq + j * c.r * inverse_magnitude_sq - k * c.i * inverse_magnitude_sq -
                 temp_r * c.j * inverse_magnitude_sq;
-            k = temp_j * c.i * inverse_magnitude_sq + k * c.r * inverse_magnitude_sq - tr * c.k * inverse_magnitude_sq -
+            k = temp_j * c.i * inverse_magnitude_sq + k * c.r * inverse_magnitude_sq - temp_r * c.k * inverse_magnitude_sq -
                 temp_i * c.j * inverse_magnitude_sq;
             return *this;
         }
@@ -130,7 +132,7 @@ namespace math {
         inline constexpr auto &operator[](const unsigned int index) const { return data[index]; }
         inline constexpr auto dot(const Complex<T, 4> &c) const { return r * c.r + i * c.i + j * c.j + k * c.k; }
         inline constexpr auto length() const {
-            return static_cast<T>(INTERNAL_MATH_SQUARE_ROOT_FUNC(sq(x) + sq(y) + sq(j) + sq(k)));
+            return static_cast<T>(INTERNAL_MATH_SQUARE_ROOT_FUNC(sq(r) + sq(i) + sq(j) + sq(k)));
         }
         inline constexpr auto &normalize() {
             T factor = static_cast<T>(1) / length();
