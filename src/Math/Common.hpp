152c99cb3fe0feac8af10e4aece9d31b8b89b4c0
GabeRundlett <gabe@rundlett.com>
2020-02-10 14:57:33 -0500

    Update

--- src/Math/Common.hpp
+++ src/Math/Common.hpp
@@ -6,7 +6,7 @@ namespace Math {
     template <typename Precision> static inline constexpr auto sq(const Precision val) { return val * val; }
     template <typename Precision> static inline constexpr auto cb(const Precision val) { return val * val * val; }
     template <typename PrecisionOut, typename PrecisionIn> static inline constexpr PrecisionOut oneOver(const PrecisionIn val) {
-        return static_cast<PrecisionOut>(1) / val;
+        return static_cast<PrecisionOut>(static_cast<PrecisionOut>(1) / val);
     }
 
     template <unsigned long long Constant> inline constexpr unsigned long long factorial() { return factorial<Constant - 1>(); }
@@ -15,27 +15,27 @@ namespace Math {
     template <typename PrecisionOut, typename PrecisionInX, typename PrecisionInM>
     static inline constexpr auto modulo(const PrecisionInX x, const PrecisionInM m) {
         if (x < static_cast<PrecisionInX>(0))
-            return static_cast<double>(static_cast<int>(x * oneOver<PrecisionOut>(m) - 1) * m - x);
+            return static_cast<PrecisionOut>(static_cast<int>(x * oneOver<PrecisionOut>(m) - 1) * m - x);
         else
-            return static_cast<double>(x - static_cast<int>(x * oneOver<PrecisionOut>(m)) * m);
+            return static_cast<PrecisionOut>(x - static_cast<int>(x * oneOver<PrecisionOut>(m)) * m);
     }
     template <typename PrecisionOut, typename PrecisionInX> static inline constexpr PrecisionOut modTau(const PrecisionInX x) {
         if (x < static_cast<PrecisionInX>(0))
-            return static_cast<double>(static_cast<int>(x * oneOver<PrecisionOut>(TAU) - 1) * TAU - x);
+            return static_cast<PrecisionOut>(static_cast<int>(x * oneOver<PrecisionOut>(TAU) - 1) * TAU - x);
         else
-            return static_cast<double>(x - static_cast<int>(x * oneOver<PrecisionOut>(TAU)) * TAU);
+            return static_cast<PrecisionOut>(x - static_cast<int>(x * oneOver<PrecisionOut>(TAU)) * TAU);
     }
     template <typename PrecisionOut, typename PrecisionInX> static inline constexpr PrecisionOut modPi(const PrecisionInX x) {
         if (x < static_cast<PrecisionInX>(0))
-            return static_cast<double>(static_cast<int>(x * oneOver<PrecisionOut>(PI) - 1) * PI - x);
+            return static_cast<PrecisionOut>(static_cast<int>(x * oneOver<PrecisionOut>(PI) - 1) * PI - x);
         else
-            return static_cast<double>(x - static_cast<int>(x * oneOver<PrecisionOut>(PI)) * PI);
+            return static_cast<PrecisionOut>(x - static_cast<int>(x * oneOver<PrecisionOut>(PI)) * PI);
     }
     template <typename PrecisionOut, typename PrecisionInX>
     static inline constexpr PrecisionOut modHalfPi(const PrecisionInX x) {
         if (x < static_cast<PrecisionInX>(0))
-            return static_cast<double>(static_cast<int>(x * oneOver<PrecisionOut>(HALF_PI) - 1) * HALF_PI - x);
+            return static_cast<PrecisionOut>(static_cast<int>(x * oneOver<PrecisionOut>(HALF_PI) - 1) * HALF_PI - x);
         else
-            return static_cast<double>(x - static_cast<int>(x * oneOver<PrecisionOut>(HALF_PI)) * HALF_PI);
+            return static_cast<PrecisionOut>(x - static_cast<int>(x * oneOver<PrecisionOut>(HALF_PI)) * HALF_PI);
     }
 } // namespace Math
